b19b8b07e2b786e7dd0506bcb9bfc4645d2cc45c
sujit995 <sujitmishra402@gmail.com>
2022-03-14 15:42:29 +0530

    created frontend UI with added auth0

---
+++ src/components/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+    domain: 'dev-1k3pu7x9.us.auth0.com',
+    clientId: 'Rvb4AGlwmK2Fd9IZxwdpoAAk4VnGCGUU',
+    callbackUrl: 'http://localhost:3000/callback'
+  };
+  
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import React from "react";
+import { Route, Router } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import './styles/App.css';
+
+import AppWrapper from "./components/AppWrapper";
+import history from "./utils/history";
+import * as serviceWorker from './serviceWorker';
+
+const mainRoutes = (
+  <Router history={history}>
+    <Route path="/" render={props => (<AppWrapper {...props} />)} />
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(mainRoutes, document.getElementById("root"));
+
+serviceWorker.unregister();
